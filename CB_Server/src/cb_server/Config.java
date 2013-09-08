b21749734982a72ca00514a6be68f6ae04fe2b58
Longri <www.longri.de@googlemail.com>
2013-09-08 21:06:23 +0200

    + fix for MapBase changes

--- CB_Server/src/cb_server/Config.java
+++ CB_Server/src/cb_server/Config.java
@@ -5,6 +5,11 @@ import CB_Utils.Config_Core;
 
 
 public class Config extends Config_Core {
+	
+	public Config(String workPath) {
+		super(workPath);
+			}
+
 	public static SettingsClass settings;
 
 	public static void Initialize(String workPath)
