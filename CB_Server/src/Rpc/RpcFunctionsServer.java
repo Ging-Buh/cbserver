91744cf907b3efcfcc46927f77d95fe380d0a9e3
Hubert Merkle <hubert.merkle@schletter.de>
2013-11-06 09:10:41 +0100

    Neue RPC Funktion zum Empfang von Änderungen von ACB

--- CB_Server/src/Rpc/RpcFunctionsServer.java
+++ CB_Server/src/Rpc/RpcFunctionsServer.java
@@ -5,14 +5,18 @@ import java.util.HashMap;
 
 import CB_Core.DAO.CacheListDAO;
 import CB_Core.DAO.LogDAO;
+import CB_Core.DAO.WaypointDAO;
 import CB_Core.DB.Database;
 import CB_Core.Settings.CB_Core_Settings;
 import CB_Core.Types.Cache;
 import CB_Core.Types.CacheList;
+import CB_Core.Types.ExportEntry;
 import CB_Core.Types.ImageEntry;
 import CB_Core.Types.LogEntry;
+import CB_RpcCore.Functions.RpcAnswer_ExportChangesToServer;
 import CB_RpcCore.Functions.RpcAnswer_GetCacheList;
 import CB_RpcCore.Functions.RpcAnswer_GetExportList;
+import CB_RpcCore.Functions.RpcMessage_ExportChangesToServer;
 import CB_RpcCore.Functions.RpcMessage_GetCacheList;
 import CB_RpcCore.Functions.RpcMessage_GetExportList;
 import cb_rpc.Functions.RpcAnswer;
@@ -105,6 +109,57 @@ public class RpcFunctionsServer {
 				RpcAnswer_GetCacheList answer = new RpcAnswer_GetCacheList(-1);
 				return answer;
 			}
+		} else if (message instanceof RpcMessage_ExportChangesToServer) {
+			RpcMessage_ExportChangesToServer msg = (RpcMessage_ExportChangesToServer) message;
+			System.out.println("Export vom ACB!!!");
+			
+			for (ExportEntry entry : msg.getExportList()) {
+				switch (entry.changeType) {
+				case Archived:
+					break;
+				case Available:
+					break;
+				case DeleteWaypoint:
+					break;
+				case Found:
+					break;
+				case NewWaypoint:
+					WaypointDAO wpdao = new WaypointDAO();
+					wpdao.WriteToDatabase(entry.waypoint);
+					break;
+				case NotArchived:
+					break;
+				case NotAvailable:
+					break;
+				case NotFound:
+					break;
+				case NotesText:
+					System.out.println("New Notes Text: " + entry.note);
+					Database.SetNote(entry.cacheId, entry.note);
+					break;
+				case NumTravelbugs:
+					break;
+				case SolverText:
+					// Change Solver Text
+					System.out.println("New Solver Text: " + entry.solver);
+					Database.SetSolver(entry.cacheId, entry.solver);
+					break;
+				case Undefined:
+					break;
+				case WaypointChanged:
+					wpdao = new WaypointDAO();
+					entry.waypoint.checkSum = 0;	// auf 0 setzen damit der WP in der DB upgedated wird
+					wpdao.UpdateDatabase(entry.waypoint);
+					break;
+				default:
+					break;
+				
+				}
+			}
+			// TODO Antwort kann Informationen über Konflikte übergeben, ebenfalls als exportList
+			// für jeden Konflikt wird ein Eintrag (ExportEntry) in dieser Liste übergeben
+			RpcAnswer answer = new RpcAnswer_ExportChangesToServer(0, null);
+			return answer;
 		}
 		return new RpcAnswer(-1);
 	}
