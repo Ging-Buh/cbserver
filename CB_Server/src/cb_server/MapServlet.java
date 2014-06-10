a667154806e20c3a06de2699ea4aacf5253db1d9
www.longri.de@googlemail.com <www.longri.de@googlemail.com>
2014-06-10 09:19:08 +0200

    change to original Mapsforge DatabaseRenderer

--- CB_Server/src/cb_server/MapServlet.java
+++ CB_Server/src/cb_server/MapServlet.java
@@ -1,10 +1,12 @@
 package cb_server;
 
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 
 import javax.imageio.ImageIO;
@@ -16,8 +18,12 @@ import javax.servlet.http.HttpServletResponse;
 import org.mapsforge.core.graphics.GraphicFactory;
 import org.mapsforge.core.graphics.TileBitmap;
 import org.mapsforge.core.model.Tile;
+import org.mapsforge.map.awt.AwtGraphicFactory;
+import org.mapsforge.map.awt.AwtResourceBitmap;
+import org.mapsforge.map.awt.AwtTileBitmap;
 import org.mapsforge.map.awt.ext_AwtGraphicFactory;
 import org.mapsforge.map.awt.ext_AwtTileBitmap;
+import org.mapsforge.map.layer.renderer.DatabaseRenderer;
 import org.mapsforge.map.layer.renderer.SERVER_DatabaseRenderer;
 import org.mapsforge.map.layer.renderer.RendererJob;
 import org.mapsforge.map.model.DisplayModel;
@@ -34,7 +40,7 @@ import de.Map.DesktopManager;
 public class MapServlet extends HttpServlet {
 	private static final long serialVersionUID = 2094731483963312861L;
 	private String greeting = "Hello World";
-	private SERVER_DatabaseRenderer databaseRenderer;
+	private DatabaseRenderer databaseRenderer;
 	private File mapFile;
 	private ExternalRenderTheme renderTheme;
 	private DisplayModel model;
@@ -56,28 +62,26 @@ public class MapServlet extends HttpServlet {
 		MF_mapDatabase.closeFile();
 		MF_mapDatabase.openFile(mapFile);
 
-		GraphicFactory Mapsforge_Factory = new ext_AwtGraphicFactory(1);
-		databaseRenderer = new SERVER_DatabaseRenderer(MF_mapDatabase, Mapsforge_Factory);
+		GraphicFactory Mapsforge_Factory = AwtGraphicFactory.INSTANCE;
+		databaseRenderer = new DatabaseRenderer(MF_mapDatabase, Mapsforge_Factory);
 		/*
 				Tile ti = new Tile(34986, 22738, (byte) 16);
 				RendererJob job = new RendererJob(ti, mapFile, renderTheme, model, 1, false);
 				TileBitmap tile = databaseRenderer.executeJob(job);
 
-				// Hi Hubert, wie willst Du das Image zurück geben?
-				// Als OutputStream? Dann wäre das der Richtige Code, um es als PNG zurück zu geben.
-
-				OutputStream os = new ByteArrayOutputStream();
-
-				try {
-					tile.compress(os);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
 				// Oder auf der Platte Speichern 
-				ext_AwtTileBitmap bmp = (ext_AwtTileBitmap) tile;
-				BufferedImage bufferedImage = bmp.getBufferedImage();
+				AwtTileBitmap bmp = (AwtTileBitmap) tile;
+			try {	
+				ByteArrayOutputStream os = new ByteArrayOutputStream();
+				
+					bmp.compress(os);
+				
+				
+				byte[] imageInByte = os.toByteArray();
+				
+				InputStream in = new ByteArrayInputStream(imageInByte);
+				BufferedImage bufferedImage = ImageIO.read(in);
+	 
 
 				File outputfile = new File("./cachebox/repository/maps/testimage.jpg");
 				try {
@@ -86,7 +90,12 @@ public class MapServlet extends HttpServlet {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-		*/
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+			*/
 	}
 
 	public MapServlet(String greeting) {
@@ -102,11 +111,6 @@ public class MapServlet extends HttpServlet {
 			Tile ti = new Tile(34986, 22738, (byte) 16);
 			RendererJob job = new RendererJob(ti, mapFile, renderTheme, model, 1, false);
 			TileBitmap tile = databaseRenderer.executeJob(job);
-
-			// Hi Hubert, wie willst Du das Image zurück geben?
-			// Als OutputStream? Dann wäre das der Richtige Code, um es als PNG zurück zu geben.
-
-			//		OutputStream os = new ByteArrayOutputStream();
 
 			try {
 				tile.compress(response.getOutputStream());
